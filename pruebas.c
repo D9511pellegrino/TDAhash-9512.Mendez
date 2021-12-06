717ec119f9c17ea7bd5daa12df667526d4d8e3c5
D9511pellegrino <52115987+D9511pellegrino@users.noreply.github.com>
2021-12-06 00:04:16 -0300

    todo funcionando

--- pruebas.c
+++ pruebas.c
@@ -1,18 +1,49 @@
+#include <string.h>
+
 #include "pa2mm.h"
 #include "src/hash.h"
+
+typedef struct consola {
+    char* modelo;
+    char* descripcion;
+} consola_t;
+
+
+bool mostrar_consola(hash_t* hash, const char* clave, void* aux){
+    if(!hash || !clave) return true;
+    char* descripcion = hash_obtener(hash, clave);
+    printf("Clave: %s = %s \n", clave, descripcion);
+    return false;
+}
+
+void destruir_string(void* string){
+    if(string)
+        //char* string_destruir = string;
+        free(string);
+}
+
+bool iterar_hasta_2(hash_t* hash,const char* clave, void* aux){
+    if(!hash || !clave) return true;
+    size_t* cantidad = aux;
+    if(*cantidad==2) return true;
+    return false;
+}
 
 void puedoCrearUnHash(){
     hash_t* h = NULL;
 
-    pa2m_afirmar((h=hash_crear(NULL, 5)), "Crear un hash con tamaño valido devuelve un hash");
+    pa2m_afirmar((h=hash_crear(NULL, 5)), "Crear un hash con tamaño 5 devuelve un hash");
     pa2m_afirmar(hash_cantidad(h)==0, "Hash tiene 0 elementos");
 
     hash_destruir(h);
 
-    pa2m_afirmar((h=hash_crear(NULL, 2)), "Crear un hash con tamaño invalido devuelve un hash");
+    pa2m_afirmar((h=hash_crear(NULL, 5)), "Crear un hash con tamaño 100 devuelve un hash");
     pa2m_afirmar(hash_cantidad(h)==0, "Hash tiene 0 elementos");
 
-    //pa2m_afirmar(!abb_buscar(a, NULL), "El elemento raiz es NULL");
+    hash_destruir(h);
+
+    pa2m_afirmar((h=hash_crear(NULL, 2)), "Crear un hash con tamaño 2 devuelve un hash");
+    pa2m_afirmar(hash_cantidad(h)==0, "Hash tiene 0 elementos");
 
     hash_destruir(h);
 }
@@ -21,33 +52,224 @@ void puedoInsertarHash(){
     hash_t* h = NULL;
     h=hash_crear(NULL, 5);
 
-    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer")==0, "Inserto clave exitosamente");
-    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer")==0, "Inserto clave exitosamente");
-    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer")==0, "Inserto clave exitosamente");
-    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer")==0, "Inserto clave exitosamente");
-    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer")==0, "Inserto clave exitosamente");
-    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer")==0, "Inserto clave exitosamente");
-    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer")==0, "Inserto clave exitosamente");
-        
+    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer")==0, "Inserto clave 1002 exitosamente");
+    pa2m_afirmar(hash_cantidad(h)==1, "Hash tiene 1 elemento");
+    pa2m_afirmar(strcmp(hash_obtener(h, "1002"), "Premium 10-Input 2-Bus Mixer")==0, "Busco y encuentro clave 1002");
+
+    pa2m_afirmar(hash_insertar(h, "1002", "Premium 10-Input 2-Bus Mixer USB")==0, "Inserto clave 1002 repetida exitosamente");
+    pa2m_afirmar(hash_cantidad(h)==1, "Hash tiene 1 elemento");
+    pa2m_afirmar(strcmp(hash_obtener(h, "1002"), "Premium 10-Input 2-Bus Mixer USB")==0, "Busco y encuentro clave 1002");
+
+    pa2m_afirmar(hash_insertar(h, "1204USB", "Premium 12-Input 2/2-Bus Mixer")==0, "Inserto clave 1204USB exitosamente");
+    pa2m_afirmar(hash_cantidad(h)==2, "Hash tiene 2 elementos");
+
+    pa2m_afirmar(hash_insertar(h, "302USB", "Premium 5-Input Mixer")==0, "Inserto clave 302USB exitosamente");
+    pa2m_afirmar(hash_cantidad(h)==3, "Hash tiene 3 elementos");
+
+    pa2m_afirmar(hash_insertar(h, "502", "Premium 5-Input 2-Bus Mixer")==0, "Inserto clave 502 exitosamente");
+    pa2m_afirmar(hash_cantidad(h)==4, "Hash tiene 4 elementos");
+
+    pa2m_afirmar(hash_insertar(h, "802", "Premium 8-Input 2-Bus Mixer")==0, "Inserto clave 802 exitosamente");
+    pa2m_afirmar(hash_cantidad(h)==5, "Hash tiene 5 elementos");
+
+    pa2m_afirmar(hash_insertar(h, "FLOW 8", "8-Input Digital Mixer")==0, "Inserto clave FLOW 8 exitosamente");
+    pa2m_afirmar(hash_cantidad(h)==6, "Hash tiene 6 elementos");
+
+    pa2m_afirmar(strcmp(hash_obtener(h, "502"), "Premium 5-Input 2-Bus Mixer")==0, "Busco y encuentro clave 502");
+
+    pa2m_afirmar(strcmp(hash_obtener(h, "1204USB"), "Premium 12-Input 2/2-Bus Mixer")==0, "Busco y encuentro clave 1204USB");
+
+    pa2m_afirmar(strcmp(hash_obtener(h, "502"), "Premium 5-Input 2-Bus Mixer")==0, "Busco y encuentro clave 502");
+
+    pa2m_afirmar(strcmp(hash_obtener(h, "302USB"), "Premium 5-Input Mixer")==0, "Busco y encuentro clave 302USB");
+
+    pa2m_afirmar(strcmp(hash_obtener(h, "FLOW 8"), "8-Input Digital Mixer")==0, "Busco y encuentro clave FLOW 8");
+
+    pa2m_afirmar(strcmp(hash_obtener(h, "802"), "Premium 8-Input 2-Bus Mixer")==0, "Busco y encuentro clave 802");
+    
+    pa2m_afirmar(!hash_obtener(h, "2000"), "Busco clave 2000 inexistente y encuentro NULL");
+
+    hash_destruir(h);
+}
+
+
+void puedoBorrarHAsh(){
+    hash_t* h = NULL;
+    h=hash_crear((hash_destruir_dato_t)destruir_string, 5);
+
+    pa2m_afirmar(hash_quitar(h, "1002")==-1, "Intento quitar clave de hash vacio devuelve -1");
+
+    char** consolas = malloc(4*sizeof(char*));
+    if(!consolas){
+        hash_destruir(h);
+        return;
+    }
+
+    consolas[0] = malloc(strlen("Premium 10-Input 2-Bus Mixer")+1);
+    if(!consolas[0]){
+        free(consolas);
+        hash_destruir(h);
+        return;
+    }
+    strcpy(consolas[0], "Premium 10-Input 2-Bus Mixer");
+
+    consolas[1] = malloc(strlen("Premium 12-Input 2/2-Bus Mixer")+1);
+    if(!consolas[1]){
+        free(consolas[0]);
+        free(consolas);
+        hash_destruir(h);
+        return;
+    }
+    strcpy(consolas[1], "Premium 12-Input 2/2-Bus Mixer");
+
+    consolas[2] = malloc(strlen("Premium 5-Input Mixer")+1);
+    if(!consolas[2]){
+        free(consolas[0]);
+        free(consolas[1]);
+        free(consolas);
+        hash_destruir(h);
+        return;
+    }
+    strcpy(consolas[2], "Premium 5-Input Mixer");
+
+    consolas[3] = malloc(strlen("Premium 5-Input 2-Bus Mixer")+1);
+    if(!consolas[3]){
+        free(consolas[0]);
+        free(consolas[1]);
+        free(consolas[2]);
+        free(consolas);
+        hash_destruir(h);
+        return;
+    }
+    strcpy(consolas[3], "Premium 5-Input 2-Bus Mixer");
+
+    pa2m_afirmar(hash_insertar(h, "1002", consolas[0])==0, "Inserto clave 1002 exitosamente");
+    pa2m_afirmar(hash_insertar(h, "1204USB", consolas[1])==0, "Inserto clave 1204USB exitosamente");
+
+    pa2m_afirmar(hash_cantidad(h)==2, "Hash tiene 2 elementos");
+
+    pa2m_afirmar(hash_quitar(h, "1002")==0, "Intento quitar clave 1002 de hash devuelve 0");
+    pa2m_afirmar(hash_quitar(h, "1002")==-1, "Intento quitar clave 1002 nuevamente de hash devuelve -1");
+
+    pa2m_afirmar(hash_cantidad(h)==1, "Hash tiene 1 elemento");
+
+    pa2m_afirmar(hash_insertar(h, "302USB", consolas[2])==0, "Inserto clave 302USB exitosamente");
+    pa2m_afirmar(hash_insertar(h, "502", consolas[3])==0, "Inserto clave 502 exitosamente");
+
+    pa2m_afirmar(hash_cantidad(h)==3, "Hash tiene 3 elementos");
+
+    pa2m_afirmar(!hash_obtener(h, "1002"), "Busco clave 1002 borrada y encuentro NULL");
+
+    pa2m_afirmar(hash_quitar(h, "1204USB")==0, "Intento quitar clave 1204USB de hash devuelve 0");
+    pa2m_afirmar(hash_quitar(h, "302USB")==0, "Intento quitar clave 302USB de hash devuelve 0");
+    pa2m_afirmar(hash_quitar(h, "502")==0, "Intento quitar clave 502 de hash devuelve 0");
+
+    pa2m_afirmar(hash_cantidad(h)==0, "Hash tiene 0 elementos");
+    
+    free(consolas);
+    hash_destruir(h);
+}
+
+
+void pruebasIteradorHash(){
+    hash_t* h = NULL;
+    h=hash_crear((hash_destruir_dato_t)destruir_string, 5);
+
+    char** consolas = malloc(4*sizeof(char*));
+    if(!consolas){
+        hash_destruir(h);
+        return;
+    }
+
+    consolas[0] = malloc(strlen("Premium 10-Input 2-Bus Mixer")+1);
+    if(!consolas[0]){
+        free(consolas);
+        hash_destruir(h);
+        return;
+    }
+    strcpy(consolas[0], "Premium 10-Input 2-Bus Mixer");
+
+    consolas[1] = malloc(strlen("Premium 12-Input 2/2-Bus Mixer")+1);
+    if(!consolas[1]){
+        free(consolas[0]);
+        free(consolas);
+        hash_destruir(h);
+        return;
+    }
+    strcpy(consolas[1], "Premium 12-Input 2/2-Bus Mixer");
+
+    consolas[2] = malloc(strlen("Premium 5-Input Mixer")+1);
+    if(!consolas[2]){
+        free(consolas[0]);
+        free(consolas[1]);
+        free(consolas);
+        hash_destruir(h);
+        return;
+    }
+    strcpy(consolas[2], "Premium 5-Input Mixer");
+
+    consolas[3] = malloc(strlen("Premium 5-Input 2-Bus Mixer")+1);
+    if(!consolas[3]){
+        free(consolas[0]);
+        free(consolas[1]);
+        free(consolas[2]);
+        free(consolas);
+        hash_destruir(h);
+        return;
+    }
+    strcpy(consolas[3], "Premium 5-Input 2-Bus Mixer");
+
+    pa2m_afirmar(hash_insertar(h, "1002", consolas[0])==0, "Inserto clave 1002 exitosamente");
+    pa2m_afirmar(hash_insertar(h, "1204USB", consolas[1])==0, "Inserto clave 1204USB exitosamente");
+    pa2m_afirmar(hash_insertar(h, "302USB", consolas[2])==0, "Inserto clave 302USB exitosamente");
+    pa2m_afirmar(hash_insertar(h, "502", consolas[3])==0, "Inserto clave 502 exitosamente");
+    
+    pa2m_afirmar(hash_con_cada_clave(h, mostrar_consola, NULL)==4, "Se mostraron 4 consolas, puedo iterar con aux NULL");
+
+    size_t cantidad_iterados = 0;
+    pa2m_afirmar(hash_con_cada_clave(h, iterar_hasta_2, &cantidad_iterados)==4, "Se iteraron cantidad_iterados consolas");
+
+
+
+    free(consolas);
+    hash_destruir(h);
+
+}
+
+void pruebasNULLHash(){
+    hash_t* h = NULL;
+
+    pa2m_afirmar((h=hash_crear(NULL, 5)), "Crear un hash con destructor NULL devuelve un hash");
+    pa2m_afirmar(hash_insertar(h, NULL, "NULL")==-1, "Insertar con clave NULL devuelve -1");
+    pa2m_afirmar(hash_cantidad(h)==0, "Hash tiene 0 elementos");
+
+    pa2m_afirmar(hash_insertar(h, "802", "Premium 8-Input 2-Bus Mixer")==0, "Inserto clave 802 exitosamente");
+    pa2m_afirmar(hash_cantidad(h)==1, "Hash tiene 1 elementos");
+
+    pa2m_afirmar(hash_quitar(h, NULL)==-1, "Intento quitar clave NULL de hash devuelve -1");
+
+    pa2m_afirmar(hash_insertar(h, "202", NULL)==0, "Inserto clave 202 con elemento NULL exitosamente");
 
 
 
     hash_destruir(h);
 }
 
-
 int main(){
     pa2m_nuevo_grupo("Pruebas de creacion Hash");
     puedoCrearUnHash();
 
-    pa2m_nuevo_grupo("Pruebas de insercion");
+    pa2m_nuevo_grupo("Pruebas de insercion y busqueda");
     puedoInsertarHash();
 
-    pa2m_nuevo_grupo("Pruebas de busqueda");
-
     pa2m_nuevo_grupo("Pruebas de borrado");
+    puedoBorrarHAsh();
 
-    pa2m_nuevo_grupo("Pruebas de recorrido");
+    pa2m_nuevo_grupo("Pruebas de iterador interno");
+    pruebasIteradorHash();
+
+    pa2m_nuevo_grupo("Pruebas con NULL");
+    pruebasNULLHash();
 
     return pa2m_mostrar_reporte();
 }
--- src/hash.c
+++ src/hash.c
@@ -5,10 +5,9 @@
 #include "hash.h"
 #include "lista.h"
 
-
-
 #define ERROR -1
 #define EXITO 0
+#define FACTOR_CARGA 0.75
 
 typedef struct elemento_hash{
     char* clave;
@@ -23,17 +22,7 @@ struct hash {
     lista_t** tabla;
 };
 
-/*
- * Crea el hash reservando la memoria necesaria para el.
- * Destruir_elemento es un destructor que se utilizará para liberar
- * los elementos que se eliminen del hash.  Capacidad indica la
- * capacidad inicial con la que se crea el hash. La capacidad inicial
- * no puede ser menor a 3. Si se solicita una capacidad menor, el hash
- * se creará con una capacidad de 3.
- *
- * Devuelve un puntero al hash creado o NULL en caso de no poder
- * crearlo.
- */
+
 hash_t* hash_crear(hash_destruir_dato_t destruir_elemento, size_t capacidad_inicial){
     if(capacidad_inicial<3) capacidad_inicial = 3;
 
@@ -53,8 +42,6 @@ hash_t* hash_crear(hash_destruir_dato_t destruir_elemento, size_t capacidad_inic
     nuevo_hash->capacidad = capacidad_inicial;
 
     return nuevo_hash;
-
-    //return NULL;
 }
 
 
@@ -62,11 +49,47 @@ size_t hash_maker(const char* clave){
     if(clave){
         size_t numero_magico = 0;
         for(int i = 0; clave[i]!='\0'; i++){
-            numero_magico += (size_t)clave[i] * i;
+            numero_magico += (size_t)clave[i] * (i+1);
         }
         return numero_magico;
     }
     return 0;
+}
+
+size_t rehash(hash_t* hash){
+
+    if(hash){
+        hash_t* hash_aux = hash_crear(hash->destructor, hash->capacidad * 2 +1); 
+        if(!hash_aux) return ERROR;
+
+        elemento_t* elemento_aux;
+        lista_iterador_t* iterador = NULL;
+
+        for(int i = 0; i<hash_aux->capacidad; i++){
+            if(!lista_vacia(hash_aux->tabla[i])){
+                iterador = lista_iterador_crear(hash_aux->tabla[i]);
+                
+                while(lista_iterador_tiene_siguiente(iterador)){
+                    elemento_aux = lista_iterador_elemento_actual(iterador);
+                    if(hash_insertar(hash_aux, elemento_aux->clave, elemento_aux->elemento)!=0){
+                        hash_destruir(hash_aux);
+                        lista_iterador_destruir(iterador);
+                        return ERROR;
+                    }
+                    lista_iterador_avanzar(iterador);  
+                }
+
+                lista_iterador_destruir(iterador);
+            }
+        }
+
+        hash_t hash_pointer = *hash; 
+        *hash = *hash_aux;
+        *hash_aux = hash_pointer;
+        hash_destruir(hash_aux);
+        return EXITO;
+    }
+    return ERROR;
 }
 
 /*
@@ -79,7 +102,9 @@ size_t hash_maker(const char* clave){
  * Devuelve 0 si pudo guardarlo o -1 si no pudo.
  */
 int hash_insertar(hash_t* hash, const char* clave, void* elemento){
-    if(!clave) return ERROR;
+    if(!clave || !hash) return ERROR;
+
+    if((hash->cantidad_listas/hash->capacidad)>FACTOR_CARGA) rehash(hash);
 
     elemento_t* elemento_nuevo_hash = malloc(sizeof(elemento_t));
     if(!elemento_nuevo_hash) return ERROR;
@@ -108,32 +133,67 @@ int hash_insertar(hash_t* hash, const char* clave, void* elemento){
     lista_insertar(hash->tabla[posicion], elemento_nuevo_hash);
     hash->cantidad_elementos++;
 
-    /*LUGAR PARA REHASH*/
+    if((hash->cantidad_listas/hash->capacidad)>FACTOR_CARGA) rehash(hash);
 
     return EXITO;
 }
 
-/*
- * Quita un elemento del hash e invoca la funcion destructora
- * pasandole dicho elemento.
- * Devuelve 0 si pudo eliminar el elemento o -1 si no pudo.
- */
+
 int hash_quitar(hash_t* hash, const char* clave){
-    return 0;
+    if(hash && clave){
+        elemento_t* elemento_aux;
+        lista_iterador_t* iterador = NULL;
+        size_t posicion;
+        for(int i = 0; i<hash->capacidad; i++){
+            if(!lista_vacia(hash->tabla[i])){
+                posicion = 0;
+                iterador = lista_iterador_crear(hash->tabla[i]);
+                while(lista_iterador_tiene_siguiente(iterador)){
+                    elemento_aux = lista_iterador_elemento_actual(iterador);
+                    if(strcmp(elemento_aux->clave, clave)==0){
+                        elemento_aux = lista_quitar_de_posicion(hash->tabla[i], posicion);
+                        free(elemento_aux->clave);
+                        if(hash->destructor) hash->destructor(elemento_aux->elemento);
+                        free(elemento_aux);
+                        lista_iterador_destruir(iterador);
+                        hash->cantidad_elementos--;
+                        return EXITO;
+                    } 
+                    lista_iterador_avanzar(iterador);
+                    posicion++;
+                }
+                lista_iterador_destruir(iterador);
+            }
+        }
+    }
+    return ERROR;
 }
 
-/*
- * Devuelve un elemento del hash con la clave dada o NULL si dicho
- * elemento no existe (o en caso de error).
- */
+
 void* hash_obtener(hash_t* hash, const char* clave){
+    if(hash && clave){
+        elemento_t* elemento_aux;
+        lista_iterador_t* iterador = NULL;
+
+        for(int i = 0; i<hash->capacidad; i++){
+            if(!lista_vacia(hash->tabla[i])){
+                iterador = lista_iterador_crear(hash->tabla[i]);
+                while(lista_iterador_tiene_siguiente(iterador)){
+                    elemento_aux = lista_iterador_elemento_actual(iterador);
+                    if(strcmp(elemento_aux->clave, clave)==0){
+                        lista_iterador_destruir(iterador);
+                        return elemento_aux->elemento;
+                    } 
+                    lista_iterador_avanzar(iterador);
+                }
+                lista_iterador_destruir(iterador);
+            }
+        }
+    }
     return NULL;
 }
 
-/*
- * Devuelve true si el hash contiene un elemento almacenado con la
- * clave dada o false en caso contrario (o en caso de error).
- */
+
 bool hash_contiene(hash_t* hash, const char* clave){
     if(hash && clave){
         elemento_t* elemento_aux;
@@ -158,65 +218,54 @@ bool hash_contiene(hash_t* hash, const char* clave){
     return false;
 }
 
-/*
- * Devuelve la cantidad de elementos almacenados en el hash o 0 en
- * caso de error.
- */
 size_t hash_cantidad(hash_t* hash){
     if(hash) return hash->cantidad_elementos;
     else return 0;
 }
 
-/*
- * Destruye el hash liberando la memoria reservada y asegurandose de
- * invocar la funcion destructora con cada elemento almacenado en el
- * hash.
- */
 void hash_destruir(hash_t* hash){
-    /*if(hash_cantidad(hash)!=0){
-        for(int i = 0; i < hash->capacidad; i++){
-            casillero_t* aux;
-            casillero_t* actual = hash->tabla[i];
-            while(actual->siguiente){
-                aux = actual;
-                actual = actual->siguiente;
-                free(aux);
+    if(hash){
+        elemento_t* dato_aux;
+        for(int i = 0; i<hash->capacidad; i++){
+            while(!lista_vacia(hash->tabla[i])){
+                dato_aux = lista_quitar(hash->tabla[i]);
+                free(dato_aux->clave);
+                if(hash->destructor) hash->destructor(dato_aux->elemento);
+                free(dato_aux);
             }
-            free(actual);
-            free(hash->tabla[i]);
+            lista_destruir(hash->tabla[i]);
         }
+        free(hash->tabla);
+        free(hash);
     }
-    else free(hash->tabla);*/
-    elemento_t* dato_aux;
-    for(int i = 0; i<hash->capacidad; i++){
-        while(!lista_vacia(hash->tabla[i])){
-            dato_aux = lista_quitar(hash->tabla[i]);
-            free(dato_aux->clave);
-            if(hash->destructor) hash->destructor(dato_aux->elemento);
-            free(dato_aux);
-        }
-        lista_destruir(hash->tabla[i]);
-    }
-    free(hash->tabla);
-    free(hash);
 }
 
 
-/*
- * Recorre cada una de las claves almacenadas en la tabla de hash e
- * invoca a la función funcion, pasandole como parámetros el hash, la
- * clave en cuestión y el puntero auxiliar.
- *
- * Mientras que queden mas claves o la funcion retorne false, la
- * iteración continúa. Cuando no quedan mas claves o la función
- * devuelve true, la iteración se corta y la función principal
- * retorna.
- *
- * Devuelve la cantidad de claves totales iteradas (la cantidad de
- * veces que fue invocada la función) o 0 en caso de error.
- *
- */
 size_t hash_con_cada_clave(hash_t* hash, bool (*funcion)(hash_t* hash, const char* clave, void* aux), void* aux){
-    return 0;
+    size_t cantidad_iterados = 0;
+    if(hash){
+        elemento_t* elemento_aux;
+        lista_iterador_t* iterador = NULL;
+
+        for(int i = 0; i<hash->capacidad; i++){
+            if(!lista_vacia(hash->tabla[i])){
+                iterador = lista_iterador_crear(hash->tabla[i]);
+                
+                while(lista_iterador_tiene_siguiente(iterador)){
+                    elemento_aux = lista_iterador_elemento_actual(iterador);
+
+                    if(funcion(hash, elemento_aux->clave, aux)){
+                        lista_iterador_destruir(iterador);
+                        return cantidad_iterados+1;
+                    }
+                    lista_iterador_avanzar(iterador);
+                    cantidad_iterados++;  
+                }
+                lista_iterador_destruir(iterador);
+            }
+        }
+
+    }
+    return cantidad_iterados;
 }
 
